473a0b9849f7cf4d00adebb8fbcb40d0fd1b7f8d
camila-a11-w <105289494+camila-a11-w@users.noreply.github.com>
2022-07-18 21:25:51 -0500

    actividad 1 js

---
+++ app.js
@@ -0,0 +1,8 @@
+let name = "Camila Sofia",
+  lastname = "Torres",
+  yearOfBirth = 17,
+  cityOfbirth = "Venezuela";
+  
+console.log(
+  `Hola, mi nombre es ${name} ${lastname}, vivo en medellin y tengo ${yearOfBirth} años`
+);
